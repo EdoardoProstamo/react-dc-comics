9b246e464e36f58e0bd6dd7018135bef14466d92
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-04 00:48:13 +0200

    feat: struttura Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
 function Footer() {
-    return <footer>Prova Footer 1</footer>
+    return <footer>
+        <div>
+            <button>SIGN-UP-NOW!</button>
+            <div>
+                <h3>FOLLOW US</h3>
+                (img)
+                (img)
+                (img)
+                (img)
+                (img)
+            </div>
+        </div>
+    </footer>
 };
 
 export default Footer;
