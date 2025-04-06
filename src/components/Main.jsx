618b6f10d34de18bf20d53f4de1ddb274f1535f1
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-06 10:24:23 +0200

    feat: main-menu-container

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,10 @@
+// importazione mini loghi
 import miniLogoDc from '../img/buy-comics-digital-comics.png'
 import logoTshirt from '../img/buy-comics-merchandise.png'
 import logoCard from '../img/buy-comics-subscriptions.png'
 import logoGps from '../img/buy-comics-shop-locator.png'
 import logoVisa from '../img/buy-dc-power-visa.svg'
-
+import bigLogo from '../img/dc-logo-bg.png'
 
 
 function Main() {
@@ -33,52 +34,56 @@ function Main() {
                 <a href="#"><article>DC POWER VISA</article></a>
             </div>
         </div>
-        <div className="main-menu">
-            <div>
-                <h3>DC COMICS</h3>
-                <ul>
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>Tv</li>
-                    <li>Games</li>
-                    <li>Collectibles</li>
-                    <li>Videos</li>
-                    <li>News</li>
-                </ul>
+        <div className="main-menu-container">
+            <div className='main-menu'>
+                <div>
+                    <h3>DC COMICS</h3>
+                    <ul>
+                        <a href="#"><li>Characters</li></a>
+                        <a href="#"><li>Comics</li></a>
+                        <a href="#"><li>Movies</li></a>
+                        <a href="#"><li>Tv</li></a>
+                        <a href="#"><li>Games</li></a>
+                        <a href="#"><li>Collectibles</li></a>
+                        <a href="#"><li>Videos</li></a>
+                        <a href="#"><li>News</li></a>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SHOP</h3>
+                    <ul>
+                        <a href="#"><li>Shop DC</li></a>
+                        <a href="#"><li>Shop DC Collectibles</li></a>
+                    </ul>
+                </div>
+                <div>
+                    <h3>DC</h3>
+                    <ul>
+                        <a href="#"><li>Therms of use</li></a>
+                        <a href="#"> <li>Pravicy Policy (New)</li></a>
+                        <a href="#"><li>Ad Choices</li></a>
+                        <a href="#"><li>Advertising</li></a>
+                        <a href="#"> <li>Jobs</li></a>
+                        <a href="#"><li>Subscriptions</li></a>
+                        <a href="#"><li>Talent Workshops</li></a>
+                        <a href="#"><li>CPSC Certificates</li></a>
+                        <a href="#"><li>Ratings</li></a>
+                        <a href="#"> <li>Shop Help</li></a>
+                        <a href="#"><li>Contact Us</li></a>
+                    </ul>
+                </div>
+                <div>
+                    <h3>SITES</h3>
+                    <ul>
+                        <a href="#"><li>DC</li></a>
+                        <a href="#"> <li>Mad Magazine</li></a>
+                        <a href="#"><li>DC Kids</li></a>
+                        <a href="#"><li>DC Universe</li></a>
+                        <a href="#"><li>DC Power Visa</li></a>
+                    </ul>
+                </div>
             </div>
-            <div>
-                <h3>SHOP</h3>
-                <ul>
-                    <li>Shop DC</li>
-                    <li>Shop DC Collectibles</li>
-                </ul>
-            </div>
-            <div>
-                <h3>DC</h3>
-                <ul>
-                    <li>Therms of use</li>
-                    <li>Pravicy Policy (New)</li>
-                    <li>Ad Choices</li>
-                    <li>Advertising</li>
-                    <li>Jobs</li>
-                    <li>Subscriptions</li>
-                    <li>Talent Workshops</li>
-                    <li>CPSC Certificates</li>
-                    <li>Ratings</li>
-                    <li>Shop Help</li>
-                    <li>Contact Us</li>
-                </ul>
-            </div>
-            <div>
-                <h3>SITES</h3>
-                <ul>
-                    <li>DC</li>
-                    <li>Mad Magazine</li>
-                    <li>DC Kids</li>
-                    <li>DC Universe</li>
-                    <li>DC Power Visa</li>
-                </ul>
+            <div className='big-logo-container'>
             </div>
         </div>
     </main>
