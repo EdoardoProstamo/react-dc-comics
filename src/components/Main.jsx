46cede01f1cbb0e97ef8ca3ac8e0d86781123595
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-04-07 23:20:38 +0200

    feat: scaffolding

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,15 @@
 import BlueSection from "../components/BlueSection"
 
-const MenuTitle = ["DC COMICS", "SHOP", "DC", "SITES"];
-const NavObject = ["Characters", "Comics", "Movies", "Tv", "Games", "Collectibles", "Videos", "Fans", "News", "Shop", "Shop DC", "Shop DC Collectibles", "Therms of use", "Pravicy Policy (New)", "Ad Choices", "Advertising", "Jobs", "Subscriptions", "Talent Workshops", "CPSC Certificates", "Ratings", "Shop Help", "Contact Us", "Mad Magazine", "DC Kids", "DC Universe", "DC Power Visa"];
-
-// const MenuObject = [""];
-
 function Main() {
     return <main>
         <div className="main-contents">
-            <a href="#" className="hover"><article> {'-->'} Content goes here {'<--'} </article></a>
+            <div className="jumbotron">
+                <article>CURRENT SERIES</article>
+            </div>
+            <div>
+                <div></div>
+                <button>LOAD MORE</button>
+            </div>
         </div>
         <BlueSection />
     </main>
